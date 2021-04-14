9a0785a7ad6b0628cbb2f4456321d7122a5d7529
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-14 19:20:13 +0300

    Update task.java

--- task.java
+++ task.java
@@ -14,10 +14,10 @@ public class task {
         Scanner scanner = new Scanner(System.in);
 
         List<String> text = new ArrayList<>();
-        Collections.addAll(text, scanner.nextLine().toLowerCase().replaceAll("[^a-z ]", "").split("\\s+"));
+       Collections.addAll(text, scanner.nextLine().toLowerCase().replaceAll("[^a-z ]", "").split("\\s+"));
+
 
         TreeMap<Integer, String> palindromes = new TreeMap<>();
-
         for (String word : text) {
             if (word.length() > 1 && palindrome(word)) {
                 palindromes.putIfAbsent(word.length(), word);
@@ -33,7 +33,6 @@ public class task {
 
 
     private static boolean palindrome(String word) {
-        StringBuilder reversWord = new StringBuilder(word).reverse();
-        return word.equals(reversWord.toString());
+        return word.equals(new StringBuilder(word).reverse().toString());
     }
 }
